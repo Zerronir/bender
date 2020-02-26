adeb310e110487ef859c583821b54246adc4f60f
Raúl Miralles Cardador <pqe0023@gmail.com>
2020-02-26 18:29:30 +0100

    Intentamos mover el robot

--- src/Bender.java
+++ src/Bender.java
@@ -2,46 +2,36 @@ import java.util.*;
 
 class Bender{
     String[][] mapa;
-    Player p;
+    Robot p;
     int[] posSalida;
     String resultado = "";
     LinkedList<Integer[]> teleport = new LinkedList<>();
     LinkedList<Integer[]> iterator = new LinkedList<>();
+
     // Constructor: ens passen el mapa en forma d'String
     public Bender(String mapa) {
         System.out.println(mapa);
+        String[][] mape = this.mapa;
 
-        String [] rows = mapa.split("\n");
-        String[] columns = new String[0];
+        //Treïem el número de files que té el mapa
+        String[] rows = mapa.split("\n");
+        char[] columns = new char[0];
 
         for (int i = 0; i < rows.length; i++) {
-            columns = columns.toCharArray();
+            columns = Arrays.toString(rows).toCharArray();
         }
 
-        this.mapa = new String[columns.length][];
-        for (int i = 0; i < columns.length; i++) {
-            this.mapa[i] = columns[i].split("");
-        }
+        char[][] map = new char[rows.length][columns.length];
+        int k = 0;
 
-        for (int i = 0; i < this.mapa.length; i++) {
-            for (int j = 0; j < this.mapa[0].length; j++) {
-                if (this.mapa[i][j].equals("X")) {
-                    this.p = new Player(new int[]{i, j});
-                }
-                if (this.mapa[i][j].equals("$")) {
-                    this.posSalida = new int[]{i, j};
-                }
-                if (this.mapa[i][j].equals("T")) {
-                    teleport.add(new Integer[]{i, j});
-                    System.out.println("There's a teleport ! " + i + " " + j);
-                }
-                if (this.mapa[i][j].equals("I")) {
-                    iterator.add(new Integer[]{i, j});
-                    System.out.println("There's a iterator!! " + i + " " + j);
+        for (int i = 0; i < rows.length; i++) {
+            for (int j = 0; j < columns.length; j++) {
+                if(this.mapa[i][j].equals("X")){
+                    System.out.println("robot a: " + i + " " + j);
+                    this.p = new Robot(new int[]{i, j});
                 }
             }
         }
-
 
     }
 
@@ -55,8 +45,11 @@ class Bender{
         String resultat = "";
         boolean llistat = true;
 
+        while(p.priority == p.priorityDir1){
+            p.toSouth(mapa);
 
-
+            return Arrays.deepToString(p.toSouth(mapa));
+        }
 
         return "";
     }
@@ -65,4 +58,39 @@ class Bender{
         return 0;
     }
 
+}
+
+class Robot {
+    int movements = 0;
+    char lastMovement = '0';
+    int[] posicio;
+    //Declaram les prioritats de les direccions
+    char[] priorityDir1 = new char[]{'S', 'E', 'N', 'W'};
+    char[] priorityDir2 = new char[]{'N', 'W', 'S', 'E'};
+    char[] priority = priorityDir1;
+
+    public Robot(int[] posicio){
+        this.posicio = posicio;
+        System.out.println("La posició del robot es: " + posicio[0] + " " + posicio[1]);
+    }
+
+    //Moure cap al sud
+    String[][] toSouth(String[][] mapa){
+        System.out.println("Moving southwards");
+
+        if(mapa[posicio[0] +1 ][posicio[1]].equals("#")){
+            return null;
+        }else{
+            mapa[posicio[0]][posicio[1]] = " ";
+            mapa[posicio[0]+1][posicio[1]] = "X";
+            posicio[0] = posicio[0] + 1;
+            lastMovement = 'S';
+            movements++;
+        }
+
+        return mapa;
+    }
+
+    public void priority() {
+    }
 }
